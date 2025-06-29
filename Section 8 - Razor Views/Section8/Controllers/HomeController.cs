70b8ef6037378db7b7b5c3144abbf01c20d82d2c
Kishore <a.kishorekumar01@gmail.com>
2025-06-29 21:19:46 +0530

    Section 8 - Razor View - Completed

---
+++ Section 8 - Razor Views/Section8/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Section8.Model;
+
+namespace Section8.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            //return View();
+            return RedirectToAction("StronglyTypedView");
+        }
+
+        /// <summary>
+        /// View with model inside view
+        /// </summary>
+        /// <returns></returns>
+        [Route("/model")]
+        public IActionResult Model()
+        {
+            ViewData["Title"] = "View With Model";
+            ViewBag.Type = "Practice";
+            ViewData["Env"] = "Local";
+            return View();
+        }
+
+        /// <summary>
+        /// Strongly Typed View - model inside controller and passed to view
+        /// </summary>
+        /// <returns></returns>
+        [Route("/stview")]
+        public IActionResult StronglyTypedView()
+        {
+            ViewData["Title"] = "Strongly Typed View";
+            List<Person> persons = new List<Person>()
+            {
+                new Person()
+                {
+                    Id = 1,
+                    Name = "Sam",
+                    Age = 25,
+                    Gender = 0,
+                    lobby = (Lobby)0
+                },
+                new Person()
+                {
+                    Id = 1,
+                    Name = "William",
+                    Age = 17,
+                    Gender = 0,
+                    lobby = (Lobby)3
+                },
+                new Person()
+                {
+                    Id = 1,
+                    Name = "Jenny",
+                    Age = 40,
+                    Gender = (Gender)1,
+                    lobby = (Lobby)1
+                }
+            }.OrderBy(x =>x.Age).ToList();
+            return View(persons);
+        }
+
+        /// <summary>
+        /// Shared View -> First it checks inside View -> Controller Name and if not present it will check in Shared
+        /// </summary>
+        /// <returns></returns>
+        [Route("/sharedview")]
+        public IActionResult SharedView()
+        {
+            ViewData["Title"] = "Shared View";
+            ViewData["BodyText"] = "Shared View -> First it checks inside View -> Controller Name and if not present it will check in Shared";
+            return View();
+        }
+
+    }
+}
---
+++ Section 8 - Razor Views/Section8/Model/Person.cs
@@ -0,0 +1,27 @@
+﻿namespace Section8.Model
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Gender Gender { get; set; }
+        public Lobby lobby { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female,
+        Others 
+    }
+
+    public enum Lobby
+    {
+        General,
+        VIP,
+        Waiting,
+        Hold
+    }
+
+}
