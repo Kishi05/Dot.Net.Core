f6c309dd6fc3f80ffeb5ff2ea769c7c18abfe77d
Kishore <a.kishorekumar01@gmail.com>
2025-07-01 01:42:19 +0530

    Section 13 - Environments - Completed

---
+++ Section 13 - Environments/Section13/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Section13.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IWebHostEnvironment _webHostEnvironment;
+
+        public HomeController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [Route("/")]
+        [Route("Dummy")]
+        public IActionResult Index()
+        {
+            //Usage of Environment in Controller with Dependency Injection
+            Dictionary<string, string> EnvValues = new Dictionary<string, string>() {
+                {nameof(_webHostEnvironment.ContentRootPath),_webHostEnvironment.ContentRootPath },
+                {nameof(_webHostEnvironment.ApplicationName),_webHostEnvironment.ApplicationName },
+                {nameof(_webHostEnvironment.EnvironmentName),_webHostEnvironment.EnvironmentName },
+                {nameof(_webHostEnvironment.WebRootPath),_webHostEnvironment.WebRootPath }
+            };
+            ViewData["Env"] = EnvValues;
+            return View();
+        }
+
+        [Route("Dummy")]
+        public IActionResult Dummy()
+        {
+            return View();
+        }
+
+    }
+}
--- Section 13 - Environments/Section13/Program.cs
+++ Section 13 - Environments/Section13/Program.cs
@@ -1,6 +1,36 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+
+app.UseStaticFiles();
+app.MapControllers();
+app.MapDefaultControllerRoute();
+
+//Usage of Environment in Launch Settings
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
 
 app.Run();
+
+/*
+ * Apart of 
+ *      1. Launch Screen, 
+ *      2. Controller
+ *      3. Tag Helper
+ *  We can also send values from machine to Application process - while build/run application using terminal
+ *  
+ *      dotnet run --no-launch-profile <- This will ignore exisitng launch settings and thus points as Production
+ *      
+ *                                                  (or)
+ *                                                  
+ *      We can set Env value in Terminal (Local memory) and App to use it
+ *      
+ *      $ENV:ASPNETCORE_ENVIRONMENT="Staging" <- No Spaces
+ *      dotnet run --no-launch-profile <- Now it will point to Staging
+ */
